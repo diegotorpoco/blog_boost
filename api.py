03a6532c7ebbf18b7f687518e04dedaedd2b458d
brunooyague <bruno.oyague.s@gmail.com>
2023-01-03 22:40:13 -0500

    Primer commit agregue mockup medium primera version

--- api.py
+++ api.py
@@ -3,23 +3,17 @@ import openai
 from constants import OPENAI_API_KEY
 # openai.api_key = os.getenv(OPENAI_API_KEY)
 
-BASE_PROMPT = "Write me a SEO optimized title and subcategories for a company that "
-
-# COMPANY_INPUT = "helps oncological patients stick through their treatment thanks to a mobile app through specialized chatbot that sets reminders for patients for pills, fast communication with doctors, allows to store information about your physical/mental state and receive medical feedback from that. "
-
-TOPIC_PROMPT = "Make it about: "
-
-# TOPIC_INPUT = "'Mental Health'. "
-
-FINAL_PROMPT = "Finally write a paragraph for every subcategory in the style of a short blogpost"
+BASE_PROMPT_1 = "Write a 5 parragraphs essay about "
+BASE_PROMPT_2 = "The essay should be linked to "
+BASE_PROMPT_3 = "which is a "
+BASE_PROMPT_4 = "Include a SEO optimized title. The style of the writing must be elegant but readable by a third grader. Separate the title from the blog."
 
 
-
-def test_v2(company_input,topic_input):
+def test_v2(company_input,topic_input,name_input):
     openai.api_key = os.getenv(OPENAI_API_KEY)
     response_final = openai.Completion.create(
     model="text-davinci-003",
-    prompt=BASE_PROMPT+company_input+TOPIC_PROMPT+topic_input+FINAL_PROMPT+'\n\n',
+    prompt=BASE_PROMPT_1+topic_input+BASE_PROMPT_2+name_input+BASE_PROMPT_3+company_input+BASE_PROMPT_4+'\n\n',
     temperature=0.9, #0.55 fue bueno
     max_tokens=512,
     top_p=1,
